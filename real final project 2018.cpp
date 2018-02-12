f0925e8719e2a56b0b84da572babc25a3a64bc6d
adlyyy3 <33884012+adlyyy3@users.noreply.github.com>
2018-02-12 09:30:51 +0800

    Add files via upload
    Updated (not complete)

---
+++ real final project 2018.cpp
@@ -0,0 +1,150 @@
+//Akmal Adli bin Ariffin A17DW0074
+//Rifki Redha bin Ahmad Rifae  A17DW2273
+//Final Project Budget Expenses Question 3
+
+#include<iostream>
+using namespace std;
+int getdays(int days);
+float getdepart(float departure);
+float getarriv(float Arrival);
+float getTicket(float ticketPrice);
+float CarRentalFare(float carRental);
+float TaxiFees(float TaxiFare);
+float HotelFees(float HotelFare);
+float getBreakfast(float breakfast);
+float getLunch(float lunch);
+float getDinner(float dinner);
+float getIdealBudget(float idealBudget);
+
+int main()
+{
+	
+	int days,totalSpent;
+	float departure  ; 
+	float Arrival ;
+	float ticketPrice;
+	float HotelFare;
+	float carRental,idealBudget;
+	idealBudget = getIdealBudget(idealBudget);
+	days = getdays(days);
+	departure = getdepart(departure);
+	Arrival = getarriv(Arrival);
+	ticketPrice = getTicket(ticketPrice);
+    HotelFare = HotelFees(HotelFare);
+    carRental =CarRentalFare(carRental);
+    
+    totalSpent = HotelFare + ticketPrice;
+    
+	cout<<"Your Ideal Budget: RM "<<idealBudget<<endl
+	    <<"Days Travel: "<<days<<endl
+        << "Departure Time (24hours): "<<departure<<endl
+        <<"Arrival Time (24hours): "<<Arrival<<endl
+        <<"Ticket Price:RM "<<ticketPrice<<endl
+        <<"Hotel Fare:RM "<<HotelFare<<endl
+        <<"Car Rental Fare:RM "<<carRental<<endl
+      //  <<"Total Spent: "
+        
+        
+        
+        if(totalSpent<=idealBudget)
+        {
+        	cout<<"Congratulations You Saved "<<idealBudget-totalSpent
+		}
+   
+        
+
+}
+int getdays(int days)
+{	
+	cout<<"Enter Number of Days: ";
+	cin>>days;
+	while(days<1)
+	{
+		cout<<"Invalid. Please Enter Again: ";
+		cin>>days;
+	}
+	return days;
+	
+}
+float getdepart(float departure)
+{
+	cout<<"Enter Departure Time (24hours): ";
+	cin>>departure;
+	while ( departure <0 or departure > 23.59)
+	{
+		cout<<"Invalid. Please enter again: "; 
+		cin>>departure; 
+	}
+
+	return departure;
+}
+float getarriv(float Arrival)
+{
+	cout<<"Enter Arrival Time: ";
+	cin>>Arrival;
+	while ( Arrival <0 or Arrival > 23.59)
+	{
+		cout<<"Invalid. Please enter again: "; 
+		cin>>Arrival; 
+	}
+	return Arrival;
+	
+}
+float getTicket(float ticketPrice)
+{
+	cout<<"Enter Air Fare:RM ";
+	cin>>ticketPrice;
+	return ticketPrice;
+	
+	
+}
+float HotelFees(float HotelFare)
+{
+	int days;
+	cout<<"Enter The Hotel Fare for whole trip: ";
+	cin>>HotelFare; 
+	return HotelFare;
+}
+float CarRentalFare(float carRental)
+{
+	cout<<"Enter Car Rental Fees:RM ";
+	cin>>carRental;
+	return carRental;
+}
+float getIdealBudget(float idealBudget)
+{
+	cout<<"Enter Your Ideal Budget: RM ";
+	cin>>idealBudget;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
