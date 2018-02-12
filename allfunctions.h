69050d67654adaa0c533d9b849d4a2e537aa54c0
adlyyy3 <33884012+adlyyy3@users.noreply.github.com>
2018-02-12 10:09:38 +0800

    Add files via upload
    Updated header file (not complete)

---
+++ allfunctions.h
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int getdays(int days);
+float getdepart(float departure);
+float getarriv(float Arrival);
+float getTicket(float ticketPrice);
+float CarRentalFare(float carRental);
+float TaxiFees(float TaxiFare);
+float HotelFees(float HotelFare);
+float getBreakfast(float breakfast);
+float getLunch(float lunch);
+float getDinner(float dinner);
+float getIdealBudget(float idealBudget);
+
+
